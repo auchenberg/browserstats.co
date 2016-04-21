7e6e8471b6789ac508a2c68b12b335baaede74a8
René Hansen <renehh@gmail.com>
2016-04-21 23:41:11 +0200

    Updates statscounter fetcher for multiple urls
    This makes the script dump both browser and mobilebrowser endpoints.

--- statscounter/get.sh
+++ statscounter/get.sh
@@ -2,44 +2,50 @@
 
 set -e
 
-url="http://gs.statcounter.com/download/browser-country/"
+urls=(
+  "http://gs.statcounter.com/download/browser-country/"
+  "http://gs.statcounter.com/download/mobilebrowser-country/"
+)
 
-let "current_month=`date "+%m"`"
-let "current_year=`date "+%Y"`"
-
+current_month=`date "+%m"`
+current_year=`date "+%Y"`
 latest_dump_month=7
 latest_dump_year=2008
 
-# find date of latest dump
-latest_dump=`ls -t dump/*.csv | head -1` 
+for url in ${urls[@]}; do
+  kind=`echo $url | cut -d '/' -f 5`
 
-if [[ -n "$latest_dump" ]]; then
-  latest_dump_month=`echo $latest_dump | awk -F '[^0-9]*' '{print $3}'`
-  latest_dump_year=`echo $latest_dump | awk -F '[^0-9]*' '{print $2}'`
-fi
+  # find date of latest dump
+  latest_dump=`ls dump/$kind*.csv | sort | tail -1` 
 
-echo -e "Last found dump is from $latest_dump_year-$latest_dump_month.\n"
+  if [[ -n "$latest_dump" ]]; then
+    latest_dump_year=`echo $latest_dump | awk -F '[^0-9]*' '{print $2}'`
+    latest_dump_month=`echo $latest_dump | awk -F '[^0-9]*' '{print $3}'`
+  fi
 
-for year in $(seq $latest_dump_year $current_year); do
-  for month in $(seq 12); do
-    # first data is from july 2008
-    if [[ "$year" -eq 2008 && "$month" -lt 7 ]]; then
-      continue
-    fi
+  echo -e "Last found dump is from $latest_dump_year-$latest_dump_month.\n"
 
-    # skip current and future months of this year
-    if [[ "$year" -eq "$current_year" && "$month" -ge "$current_month" ]]; then
-      continue
-    fi
+  for year in $(seq $latest_dump_year $current_year); do
+    for month in $(seq 12); do
+      # first data is from july 2008
+      if [[ "$year" -eq 2008 && "$month" -lt 7 ]]; then
+        continue
+      fi
 
-    filename="dump/stats-$year-$month.csv" 
+      # skip current and future months of this year
+      if [[ "$year" -eq "$current_year" && "$month" -ge "$current_month" ]]; then
+        continue
+      fi
 
-    if [[ ! -f "$filename" ]]; then
-      echo -n "Downloading $filename... "
+      filename="dump/$kind-$year-$month.csv" 
 
-      curl -L -o $filename "$url?year=$year&month=$month"
+      if [[ ! -f "$filename" ]]; then
+        echo -n "Downloading $filename... "
 
-      echo "Done!"
-    fi
+        curl -s -L -o $filename "$url?year=$year&month=$month"
+
+        echo "Done!"
+      fi
+    done
   done
 done
